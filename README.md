238ce1a06b1e7f664742ee62a6cfdf305297e3bb
Marek Sokol <devmareksokol@gmail.com>
2024-03-04 20:47:00 +0100

    feat(client): template init

---
+++ README.md
@@ -0,0 +1,3 @@
+# OSU_Bachelor_Thesis
+
+OSU Bachelor's Thesis "Digitization of school timetables for teaching rooms" (2023/24)
---
+++ client/main/main.c
@@ -0,0 +1,15 @@
+/**
+ * @file main.c
+ * @brief Main application file.
+ */
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+static const char *DISPLAY_TAG = "main.c";
+
+void app_main(void)
+{
+    ESP_LOGI(DISPLAY_TAG, "run");
+}
