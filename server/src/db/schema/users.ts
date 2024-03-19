7caf35370e63666114be0b25b39b8566501355a5
Marek Sokol <devmareksokol@gmail.com>
2024-03-19 15:03:04 +0100

    feat(server): add profile image to user

--- server/src/db/schema/users.ts
+++ server/src/db/schema/users.ts
@@ -7,6 +7,7 @@ export const users = pgTable("users", {
     name: text("name").notNull(),
     email: text("email").unique().notNull(),
     password: text("password").notNull(),
+    profileImage: text("profileImage"),
 });
 
 export type SelectUser = InferSelectModel<typeof users>;
