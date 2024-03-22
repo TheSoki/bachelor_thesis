8444aeeb1bd26f974bbc8fed355c447e2771dd9e
Marek Sokol <devmareksokol@gmail.com>
2024-03-22 19:28:55 +0100

    fix(server): allow empty password for edit user

--- server/src/server/schema/user.ts
+++ server/src/server/schema/user.ts
@@ -14,7 +14,23 @@ export const updateUserSchema = z.object({
     id: z.string(),
     name: z.string().min(3).max(255),
     email: z.string().email().min(3).max(255),
-    password: z.string().min(3).max(255).optional(),
+    password: z
+        .string()
+        // .min(3)
+        .max(255)
+        .optional()
+        .refine(
+            (data) => {
+                // if password is not empty, it should be at least 3 characters long
+                if (!data) {
+                    return true;
+                }
+                return data.length > 2;
+            },
+            {
+                message: "String must contain at least 3 character(s)",
+            },
+        ),
 });
 
 export const registerSchema = z.object({
